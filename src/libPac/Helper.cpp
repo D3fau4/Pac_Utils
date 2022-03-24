1bce8c0b75e08070a547c6753ac322fa48defa90
D3fau4 <raulwii2@gmail.com>
2022-03-24 18:47:26 +0100

    Added the option to unpack

--- src/libPac/Helper.cpp
+++ src/libPac/Helper.cpp
@@ -2,16 +2,12 @@
 // Created by raul2 on 23/03/2022.
 //
 
-#include <filesystem>
-#include <fstream>
-#include "pac.h"
-#include "systemfilesource.h"
-#include "defines.h"
+#include "Helper.h"
 
 namespace fs = std::filesystem;
 
 static fs::path
-path_make_relative(const fs::path& from, const fs::path& to)
+path_make_relative(const fs::path &from, const fs::path &to)
 {
     auto from_iter = from.begin();
     auto to_iter = to.begin();
@@ -38,10 +34,11 @@ path_make_relative(const fs::path& from, const fs::path& to)
     return final_path;
 }
 
-EXPORTS void testlib(const char* s) {
+EXPORTS void testlib(const char *s)
+{
 
-    const fs::path& path = s;
-    std::ofstream outfile ("test.txt");
+    const fs::path &path = s;
+    std::ofstream outfile("test.txt");
 
     outfile << path.string() << std::endl;
 
@@ -49,9 +46,9 @@ EXPORTS void testlib(const char* s) {
 }
 
 EXPORTS void
-patch_archive(const char* s)
+patch_archive(const char *s)
 {
-    const fs::path& path = s;
+    const fs::path &path = s;
     fs::path root = path.parent_path();
     fs::path base = path.stem();
 
@@ -74,13 +71,12 @@ patch_archive(const char* s)
         fs::rename(target, bak_file);
     }
 
-
     lib_pac::pac_archive archive(bak_file.string());
 
     fs::recursive_directory_iterator iter(path);
 
     int n_repl = 0;
-    for (auto& it : iter)
+    for (auto &it : iter)
     {
         if (fs::is_regular_file(it))
         {
@@ -91,14 +87,85 @@ patch_archive(const char* s)
                 auto ptr = std::make_unique<lib_pac::system_file_source>(it.path().string());
                 archive.insert(virt_path.string(), std::move(ptr));
             }
-            else
-            {
-
-            }
         }
     }
 
     const auto save_info = archive.save(target.string(), nullptr);
 
     const float ratio = (save_info.compressed_size + save_info.header_size) * 100.f / (save_info.original_size);
+}
+
+EXPORTS void
+extract_archive(const char *s)
+{
+    fs::path path = s;
+    memory_buffer comp_buffer;
+    memory_buffer dec_buffer;
+
+    std::wcout << L"Extracting Archive: " << path.filename() << std::endl;
+    lib_pac::pac_archive archive(path.string());
+
+    const int n_files = archive.num_files();
+    const int f_digits = ceil(log10(n_files));
+
+    int cur_file = 0;
+
+    for (std::basic_string<char> elem: archive) {
+        const fs::path v_path = path.stem().append(elem);
+        auto file_source = archive.get(elem);
+
+        size_t dec_sz;
+
+        std::cout << "[" << std::setw(f_digits) << cur_file + 1 << "/" << n_files << "] " << v_path << std::endl;
+        if (file_source->compressed()) {
+            comp_buffer.reserve(file_source->data_size());
+
+            file_source->copy_data(comp_buffer.data(), 0, file_source->data_size());
+
+            const auto dec_info = lib_pac::compressor::prepare_decompression(comp_buffer.data(),
+                                                                             file_source->data_size());
+
+            dec_sz = dec_info->output_size();
+            const uint32_t expected_size = file_source->unpacked_size();
+
+            if (dec_sz != expected_size) {
+                std::cerr << "Size Mismatch: " << v_path;
+                std::cerr << " - Expected " << expected_size << " got " << dec_sz << std::endl;
+            }
+
+            dec_buffer.reserve(dec_sz);
+
+            lib_pac::compressor::decompress(*dec_info, dec_buffer.data());
+        } else {
+            dec_sz = file_source->unpacked_size();
+            dec_buffer.reserve(dec_sz);
+
+            file_source->copy_data(dec_buffer.data(), 0, dec_sz);
+        }
+        fs::create_directories(v_path.parent_path());
+
+#if 0
+        HANDLE hFile = CreateFileA(v_path.string().c_str(), GENERIC_WRITE, 0, nullptr, CREATE_NEW,
+        FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN | FILE_FLAG_WRITE_THROUGH,
+        nullptr);
+
+    WriteFile(hFile, dec_buffer.data(), dec_sz, nullptr, nullptr);
+    CloseHandle(hFile);
+#endif
+#if 0
+        std::ofstream output(v_path, std::ios::trunc | std::ios::binary);
+    output.write(dec_buffer.data(), dec_sz);
+    output.flush();
+    output.close();
+#endif
+#if 1
+        FILE *f;
+        f = fopen(v_path.string().c_str(), "wb");
+        fwrite(dec_buffer.data(), 1, dec_sz, f);
+        fflush(f);
+        fclose(f);
+#endif
+
+        cur_file++;
+    }
 }
--- src/libPac/Helper.h
+++ src/libPac/Helper.h
@@ -4,9 +4,18 @@
 
 #ifndef PAC_UTILS_HELPER_H
 #define PAC_UTILS_HELPER_H
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include "defines.h"
+#include "membuf.h"
+#include "pac.h"
+#include "systemfilesource.h"
+#include "defines.h"
+#include "compressor.h"
 
 EXPORTS void testlib(const char* s);
 EXPORTS void patch_archive(const char* s);
-
+EXPORTS void extract_archive(const char *s);
 #endif //PAC_UTILS_HELPER_H
