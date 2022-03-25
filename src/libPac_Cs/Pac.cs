3703ab086357bf4f736c170e016a6aa46896f579
D3fau4 <raulwii2@gmail.com>
2022-03-25 18:29:40 +0100

    Move libPac for linux to libNep

---
+++ src/libPac_Cs/Pac.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libPac_Cs
+{
+    public static class Pac
+    {
+        [DllImport(LibPac.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "_Z15extract_archivePKc")]
+        public static extern void extract_archive(String path);
+
+        [DllImport(LibPac.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "_Z12pack_archivePKc")]
+        public static extern void pack_archive(String path);
+
+        [DllImport(LibPac.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "_Z13patch_archivePKc")]
+        public static extern void patch_archive(String path);
+
+#if UNITY_EDITOR || DEBUG
+        [DllImport(LibPac.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "_Z8testpathPKc")]
+        public static extern void testpath(String path);
+
+        [DllImport(LibPac.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            EntryPoint = "_Z7testlibPKc")]
+        public static extern void testlib(String path);
+#endif
+    }
+}
